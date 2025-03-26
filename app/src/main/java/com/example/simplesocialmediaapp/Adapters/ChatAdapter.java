019ef328603ab3b0411fbf35d413bdda3327a65e
aqmal1 <71642498+aqmal1@users.noreply.github.com>
2025-03-26 17:03:19 +0800

    Visual stuff for chat
    User sends message: Will have blue background and it will be right-aligned
    
    User receives message: Will have grey background and it will be left-aligned

--- app/src/main/java/com/example/simplesocialmediaapp/Adapters/ChatAdapter.java
+++ app/src/main/java/com/example/simplesocialmediaapp/Adapters/ChatAdapter.java
@@ -1,13 +1,17 @@
 package com.example.simplesocialmediaapp.Adapters;
 
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.simplesocialmediaapp.Models.MessageModel;
 import com.example.simplesocialmediaapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.DateFormat;
 import java.util.List;
@@ -32,6 +36,19 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
     public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
         MessageModel message = messages.get(position);
         holder.textMessage.setText(message.getMessage());
+
+        // Check if the message is sent or received and set gravity and background accordingly
+        if (message.getSenderId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+            // Sent message (light blue background, right-aligned)
+            holder.textMessage.setBackgroundColor(Color.parseColor("#D0E7FF"));
+            ((LinearLayout.LayoutParams) holder.textMessage.getLayoutParams()).gravity = Gravity.END;
+            ((LinearLayout.LayoutParams) holder.textTimestamp.getLayoutParams()).gravity = Gravity.END;
+        } else {
+            // Received message (white background, left-aligned)
+            holder.textMessage.setBackgroundColor(Color.parseColor("#EDEDEB"));
+            ((LinearLayout.LayoutParams) holder.textMessage.getLayoutParams()).gravity = Gravity.START;
+            ((LinearLayout.LayoutParams) holder.textTimestamp.getLayoutParams()).gravity = Gravity.START;
+        }
 
         // Convert timestamp to a readable date/time
         String dateString = DateFormat.getDateTimeInstance().format(message.getTimestamp());
